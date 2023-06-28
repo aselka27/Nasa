31ba1c543854a17824ed14aa38a6b92386253d7a
aselka27 <asjumalieva@gmail.com>
2023-06-28 00:26:58 -0500

    Initial Commit

---
+++ Nasa/NasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaApp.swift
+//  Nasa
+//
+//  Created by саргашкаева on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
